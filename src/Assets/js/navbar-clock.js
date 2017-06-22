b9dfeb435311d8abac7e2670bb715c937ad45eda
Rahul makhija <rahulmkhj@gmail.com>
2017-06-22 21:03:38 +0530

    added jquery/navbar-clock javascripts.

---
+++ src/Assets/js/navbar-clock.js
@@ -0,0 +1,19 @@
+var monthNames = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+function startTime() {
+    var today = new Date();
+    var d = today.getDate();
+    var h = today.getHours();
+    var i = today.getMinutes();
+    var s = today.getSeconds();
+    var m = monthNames[today.getMonth()];
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('navbar-clock').innerHTML =
+        d +' ' + m + " | " + h + ":" + i + ":" + s;
+    var t = setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+$('select').selectpicker();
