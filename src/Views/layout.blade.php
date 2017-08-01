92c4c543a324be63995e2535d344de619695acc8
Rahul makhija <rahulmkhj@gmail.com>
2017-08-01 20:28:32 +0530

    added new layout view.

---
+++ src/Views/layout.blade.php
@@ -0,0 +1,20 @@
+@extends('Base::canvas')
+@section('main-content')
+    <div class="box">
+        <div class="box-header">
+            @yield('box-header-navbar')
+            <h3 class="box-title">
+                @yield('box-header')
+            </h3>
+            <span class="pull-right">
+                    @yield('box-header-right')
+            </span>
+        </div>
+        <div class="box-body">
+            @yield('content')
+        </div>
+        <div class="box-footer">
+            @yield("box-footer")
+        </div>
+    </div> <!-- /.box -->
+@endsection
