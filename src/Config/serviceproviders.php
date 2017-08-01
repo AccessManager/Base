2f093623c9fc52b9b7ce160ef967b13c90f58cb8
Rahul makhija <rahulmkhj@gmail.com>
2017-08-01 21:37:59 +0530

    cleaned up BaseServiceProvider by moving package service providers and class aliases to separate files.

---
+++ src/Config/serviceproviders.php
@@ -0,0 +1,45 @@
+<?php
+return [
+    /*
+     * Laravel Service Providers.
+     */
+    \Illuminate\Auth\AuthServiceProvider::class,
+    \Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
+    \Illuminate\Broadcasting\BroadcastServiceProvider::class,
+    \Illuminate\Bus\BusServiceProvider::class,
+    \Illuminate\Cache\CacheServiceProvider::class,
+    \Illuminate\Cookie\CookieServiceProvider::class,
+    \Illuminate\Database\DatabaseServiceProvider::class,
+    \Illuminate\Encryption\EncryptionServiceProvider::class,
+    \Illuminate\Filesystem\FilesystemServiceProvider::class,
+    \Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+    \Illuminate\Foundation\Providers\FoundationServiceProvider::class,
+    \Illuminate\Hashing\HashServiceProvider::class,
+    \Illuminate\Mail\MailServiceProvider::class,
+    \Illuminate\Notifications\NotificationServiceProvider::class,
+    \Illuminate\Pagination\PaginationServiceProvider::class,
+    \Illuminate\Pipeline\PipelineServiceProvider::class,
+    \Illuminate\Queue\QueueServiceProvider::class,
+    \Illuminate\Redis\RedisServiceProvider::class,
+    \Illuminate\Session\SessionServiceProvider::class,
+    \Illuminate\Support\ServiceProvider::class,
+    \Illuminate\Translation\TranslationServiceProvider::class,
+    \Illuminate\Validation\ValidationServiceProvider::class,
+    \Illuminate\View\ViewServiceProvider::class,
+
+
+    /*
+     * Third party package Service Providers.
+     */
+    \Collective\Html\HtmlServiceProvider::class,
+
+
+    /*
+     * Access Manager Modules Service Providers.
+     */
+    \AccessManager\Accounts\Providers\AccountsServiceProvider::class,
+    \AccessManager\Services\Providers\ServicesServiceProvider::class,
+    \AccessManager\Setup\Providers\SetupServiceProvider::class,
+    \AccessManager\Auth\Providers\AuthServiceProvider::class,
+    \AccessManager\Base\Providers\AppServiceProvider::class,
+];
--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -2,136 +2,59 @@
 
 namespace AccessManager\Base\Providers;
 
-
-use Collective\Html\FormFacade;
-use Collective\Html\HtmlFacade;
-use Collective\Html\HtmlServiceProvider;
-use Illuminate\Auth\AuthServiceProvider as LaravelAuthServiceProvider;
-use Illuminate\Auth\Passwords\PasswordResetServiceProvider;
-use Illuminate\Broadcasting\BroadcastServiceProvider;
-use Illuminate\Bus\BusServiceProvider;
-use Illuminate\Cache\CacheServiceProvider;
-use Illuminate\Cookie\CookieServiceProvider;
-use Illuminate\Database\DatabaseServiceProvider;
-use Illuminate\Encryption\EncryptionServiceProvider;
-use Illuminate\Filesystem\FilesystemServiceProvider;
 use Illuminate\Foundation\AliasLoader;
-use Illuminate\Foundation\Providers\ConsoleSupportServiceProvider;
-use Illuminate\Foundation\Providers\FoundationServiceProvider;
-use Illuminate\Hashing\HashServiceProvider;
-use Illuminate\Mail\MailServiceProvider;
-use Illuminate\Notifications\NotificationServiceProvider;
-use Illuminate\Pagination\PaginationServiceProvider;
-use Illuminate\Pipeline\PipelineServiceProvider;
-use Illuminate\Queue\QueueServiceProvider;
-use Illuminate\Redis\RedisServiceProvider;
-use Illuminate\Session\SessionServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Translation\TranslationServiceProvider;
-use Illuminate\Validation\ValidationServiceProvider;
-use Illuminate\View\ViewServiceProvider;
 
 class BaseServiceProvider extends ServiceProvider
 {
-    protected $laravelServiceProviders = [
-        LaravelAuthServiceProvider::class,
-        BroadcastServiceProvider::class,
-        BusServiceProvider::class,
-        CacheServiceProvider::class,
-        ConsoleSupportServiceProvider::class,
-        CookieServiceProvider::class,
-        DatabaseServiceProvider::class,
-        EncryptionServiceProvider::class,
-        FilesystemServiceProvider::class,
-        FoundationServiceProvider::class,
-        HashServiceProvider::class,
-        MailServiceProvider::class,
-        NotificationServiceProvider::class,
-        PaginationServiceProvider::class,
-        PipelineServiceProvider::class,
-        QueueServiceProvider::class,
-        RedisServiceProvider::class,
-        PasswordResetServiceProvider::class,
-        SessionServiceProvider::class,
-        TranslationServiceProvider::class,
-        ValidationServiceProvider::class,
-        ViewServiceProvider::class,
-    ];
-
-    protected $applicationServiceProviders = [
-        AppServiceProvider::class,
-        LaravelAuthServiceProvider::class,
-        BroadcastServiceProvider::class,
-        \AccessManager\Auth\Providers\AuthServiceProvider::class,
-        HtmlServiceProvider::class,
-    ];
-
-    protected $classAliases = [
-        'App' => \Illuminate\Support\Facades\App::class,
-        'Artisan' => \Illuminate\Support\Facades\Artisan::class,
-        'Auth' => \Illuminate\Support\Facades\Auth::class,
-        'Blade' => \Illuminate\Support\Facades\Blade::class,
-        'Broadcast' => \Illuminate\Support\Facades\Broadcast::class,
-        'Bus' => \Illuminate\Support\Facades\Bus::class,
-        'Cache' => \Illuminate\Support\Facades\Cache::class,
-        'Config' => \Illuminate\Support\Facades\Config::class,
-        'Cookie' => \Illuminate\Support\Facades\Cookie::class,
-        'Crypt' => \Illuminate\Support\Facades\Crypt::class,
-        'DB' => \Illuminate\Support\Facades\DB::class,
-        'Eloquent' => \Illuminate\Database\Eloquent\Model::class,
-        'Event' => \Illuminate\Support\Facades\Event::class,
-        'File' => \Illuminate\Support\Facades\File::class,
-        'Gate' => \Illuminate\Support\Facades\Gate::class,
-        'Hash' => \Illuminate\Support\Facades\Hash::class,
-        'Lang' => \Illuminate\Support\Facades\Lang::class,
-        'Log' => \Illuminate\Support\Facades\Log::class,
-        'Mail' => \Illuminate\Support\Facades\Mail::class,
-        'Notification' => \Illuminate\Support\Facades\Notification::class,
-        'Password' => \Illuminate\Support\Facades\Password::class,
-        'Queue' => \Illuminate\Support\Facades\Queue::class,
-        'Redirect' => \Illuminate\Support\Facades\Redirect::class,
-        'Redis' => \Illuminate\Support\Facades\Redis::class,
-        'Request' => \Illuminate\Support\Facades\Request::class,
-        'Response' => \Illuminate\Support\Facades\Response::class,
-        'Route' => \Illuminate\Support\Facades\Route::class,
-        'Schema' => \Illuminate\Support\Facades\Schema::class,
-        'Session' => \Illuminate\Support\Facades\Session::class,
-        'Storage' => \Illuminate\Support\Facades\Storage::class,
-        'URL' => \Illuminate\Support\Facades\URL::class,
-        'Validator' => \Illuminate\Support\Facades\Validator::class,
-        'View' => \Illuminate\Support\Facades\View::class,
-        'Form' =>   FormFacade::class,
-        'HTML' => HtmlFacade::class,
-    ];
 
     public function boot()
     {
         /*
-         * Register Laravel Framework Service Providers...
+         * Register Service Providers.
          */
-        foreach ($this->laravelServiceProviders as $serviceProvider )
-        {
-            $this->app->register( $serviceProvider );
-        }
-
-        /*
-         * Register Application Service Providers...
-         */
-        foreach ($this->applicationServiceProviders as $serviceProvider )
-        {
-            $this->app->register( $serviceProvider );
-        }
+        $this->registerServiceProviders();
 
         /*
          * Register Alises for Facade classes.
          */
+        $this->registerAliases();
 
-        $loader = AliasLoader::getInstance();
-        foreach ($this->classAliases as $alias => $class )
+        /*
+         * Load other resources.
+         */
+        $this->loadResources();
+
+        /*
+         * Set default string length for MySQL Queries.
+         */
+        Schema::defaultStringLength(191);
+    }
+
+    protected function registerServiceProviders()
+    {
+        $providersArray = require __DIR__ .  "/../Config/serviceproviders.php";
+
+        foreach ($providersArray as $serviceProvider )
         {
-            $loader->alias( $alias, $class );
+            $this->app->register( $serviceProvider );
         }
+    }
 
+    protected function registerAliases()
+    {
+        $loader = AliasLoader::getInstance();
+        $aliases = require __DIR__ . "/../Config/aliases.php";
+
+        foreach ($aliases as $alias => $class )
+        {
+            $loader->alias($alias, $class);
+        }
+    }
+
+    protected function loadResources()
+    {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         $this->loadViewsFrom(__DIR__ .'/../Views', 'Base');
 
@@ -140,5 +63,4 @@ class BaseServiceProvider extends ServiceProvider
             'public'
         ]);
     }
-
 }
